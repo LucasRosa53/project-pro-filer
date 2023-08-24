7632e8d36ae1d9da2d1256c3309475cea35680ae
LucasRosa53 <lucasrosa223@hotmail.com>
2023-08-24 17:06:31 -0300

    arrumando lint

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,8 +1,10 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
 
+
 def test_show_preview(capsys):
     context = {
-         "all_files": ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
+         "all_files": ["src/__init__.py",
+                       "src/app.py", "src/utils/__init__.py"],
          "all_dirs": ["src", "src/utils"]
     }
 
@@ -11,7 +13,8 @@ def test_show_preview(capsys):
 
     expected_output = (
         "Found 3 files and 2 directories\n"
-        "First 5 files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py']\n"
+        "First 5 files: ['src/__init__.py', "
+        "'src/app.py', 'src/utils/__init__.py']\n"
         "First 5 directories: ['src', 'src/utils']\n"
     )
 
