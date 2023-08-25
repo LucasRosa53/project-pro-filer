7665d156e9651b0fbb6b29f8181bc4c7a522f6b0
LucasRosa53 <lucasrosa223@hotmail.com>
2023-08-25 00:46:32 -0300

    arrumando req 3 fazendo tudo que faltou kk

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,1 +1,23 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
+
+
+def test_show_details(capsys):
+    context = {
+        "base_path": "/home/trybe/Downloads/Trybe_logo.png"
+    }
+
+    show_details(context)
+    output = capsys.readoutter()
+
+    expected_output = (
+    "File name: Trybe_logo.png"
+    "File size in bytes: 22438"
+    "File type: file"
+    "File extension: .png"
+    "Last modified date: 2023-06-13"
+    )
+
+    assert output.out == expected_output
+
+
+
--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -19,3 +19,49 @@ def test_show_preview(capsys):
     )
 
     assert output.out == expected_output
+
+
+def test_show_preview_empty(capsys):
+    context = {
+        "all_files": [],
+        "all_dirs": []
+    }
+
+    show_preview(context)
+    output = capsys.readouterr()
+
+    expected_output = "Found 0 files and 0 directories\n"
+
+
+    assert output.out == expected_output
+
+
+def test_a_lot_of_files_dirs(capsys):
+    context = {
+        "all_files": [
+            "file1.py",
+            "file2.py",
+            "file3.py",
+            "file4.py",
+            "file5.py",
+            "file6.py",
+        ],
+        "all_dirs": [
+            "dirs1",
+            "dirs2",
+            "dirs3",
+            "dirs4",
+            "dirs5",
+            "dirs6",
+        ]
+    }
+    show_preview(context)
+    output = capsys.readouterr()
+
+    expected_output = (
+        "Found 6 files and 6 directories\n"
+        "First 5 files: ['file1.py', 'file2.py', 'file3.py', 'file4.py', 'file5.py']\n"
+        "First 5 directories: ['dirs1', 'dirs2', 'dirs3', 'dirs4', 'dirs5']\n"
+    )
+
+    assert output.out == expected_output
