1b9788da61fb5854963dccbee5e05f1e37c4dff6
LucasRosa53 <lucasrosa223@hotmail.com>
2023-08-25 03:35:17 -0300

    req 4 eu acho

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,23 +1,37 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
+from unittest.mock import Mock, patch
 
-
-def test_show_details(capsys):
+def test_show_details(capsys, tmp_path):
+    file = tmp_path / "detailsFile"
+    file.touch()
     context = {
-        "base_path": "/home/trybe/Downloads/Trybe_logo.png"
+        "base_path": str(file)
     }
-
     show_details(context)
-    output = capsys.readoutter()
+    output = capsys.readouterr()
 
     expected_output = (
-    "File name: Trybe_logo.png"
-    "File size in bytes: 22438"
-    "File type: file"
-    "File extension: .png"
-    "Last modified date: 2023-06-13"
-    )
+        "File name: detailsFile\n"
+        "File size in bytes: 0\n"
+        "File type: file\n"
+        "File extension: [no extension]\n"
+        "Last modified date: 2023-08-25\n"
+        )
+
 
     assert output.out == expected_output
 
+def test_no_existance_file(capsys):
+    context = {
+        "base_path": "/home/trybe/Downloads/????"
+    }
+    mock = Mock(return_value = False)
+    with patch("os.path.exists", mock):
+        show_details(context)
+        output = capsys.readouterr()
 
+        expected_out = (
+            "File '????' does not exist\n"
+        )
 
+        assert output.out == expected_out
